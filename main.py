2675b15d04537553eff9af1fe94265e1908e3ebe
Joerg <joerg@jwcup.wds-playground.de>
2024-02-25 14:53:21 +0100

    Bug fixed in main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import datetime
 import socket
 import os
+import getpass
 
 if __name__ == "__main__":
     ts = datetime.datetime.now()
     home_dir = os.environ["HOME"]
-    user = os.environ["USERNAME"]
+    user = getpass.getuser()
     hostname = socket.gethostname()
     local_ip = socket.gethostbyname(hostname)
     # this is the base for creating logs with this app (can be configured to log SSH requests, file moves, network requests
-    print("{}: {}@{} ip: {} home-dir: {}".format(ts, user, hostname, home_dir, local_ip))
+    print("{}: {}@{} ip: {} home-dir: {}".format(ts, user, hostname, local_ip, home_dir))
+    #print("{}: ip: {} home-dir: {} hostname: {}".format(ts, local_ip, home_dir, hostname))
 
